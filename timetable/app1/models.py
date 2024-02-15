502c2fa86554c0dd6b850f90d64fa1e7838b2f5e
Nallabothu Pravallika <145707935+pravallikaa000@users.noreply.github.com>
2024-02-15 14:09:16 +0530

    Add files via upload

--- timetable/app1/models.py
+++ timetable/app1/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+  student_id = models.CharField(max_length = 20,primary_key = True)
+  student_password = models.CharField(max_length = 20)
+
+  def display(self):
+     return f"{self.student_id} {self.student_password}"
+
+  def _str_(self):
+     return self.display()
